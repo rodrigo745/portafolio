bb232226fc479c8b5f3710d5ecde17feb5793214
Rodrigo745 <roodrigoquinteros@gmail.com>
2024-09-04 21:00:07 -0300

    mejora del nav

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -4,11 +4,14 @@ import { useState } from "react";
 
 export default function Nav(){
     
+    const enlaces = ["/", "/pages/curriculum", "/pages/portafolio", "/pages/blog", "/pages/contacto"];
+    const nombres = ["Sobre mi", "Curriculum", "Portafolio", "Blog", "Contacto"];
     const [ a, setA ] = useState(true);
     const [ b, setB ] = useState(false);
     const [ c, setC ] = useState(false);
     const [ d, setD ] = useState(false);
     const [ e, setE ] = useState(false);
+    const variables = [a,b,c,d,e];
 
     const addColor = (e)=>{
         e.target.id == 0 ? setA(true) : setA(false);
@@ -22,15 +25,17 @@ export default function Nav(){
         <div>
             {/* Desktop */}
             <div className="absolute right-0 top-0 w-[60%] hidden lg:flex justify-between bg-neutral-700 shadow-md rounded-bl-lg rounded-tr-lg p-3 px-4">
-            <Link id="0" className={a ? "text-yellow-600" : ""} onClick={addColor} href="/">Sobre mi</Link>
-            <Link id="1" className={b ? "text-yellow-600" : ""} onClick={addColor}  href="/pages/curriculum">Curriculum</Link> 
-            <Link id="2" className={c ? "text-yellow-600" : ""} onClick={addColor}  href="/pages/portafolio">Portafolio</Link> 
-            <Link id="3" className={d ? "text-yellow-600" : ""} onClick={addColor}  href="/pages/blog">Blog</Link>
-            <Link id="4" className={e ? "text-yellow-600" : ""} onClick={addColor}  href="/pages/contacto">Contacto</Link>
+                {
+                    enlaces.map((es, i)=> (
+                        <Link key={i} id={i} className={variables[i] ? "text-yellow-600" : ""} onClick={addColor} 
+                        href={es}>{nombres[i]}</Link>
+                    ))
+                }
             </div>
             {/* Movil */}
             <div className="block lg:hidden absolute top-0 right-0 p-4">
                 <button className="rotate-90 font-bold text-xl">|||</button>
+                
             </div>
         </div>
     )
